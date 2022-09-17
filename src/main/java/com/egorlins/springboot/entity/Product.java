bc3fc8afa1a7e71ffd12fdc01c845dbf483e8db2
Jegor <egorlins@hotmail.com>
2022-09-17 16:59:47 +0300

    customize table structure

--- src/main/java/com/egorlins/springboot/entity/Product.java
+++ src/main/java/com/egorlins/springboot/entity/Product.java
@@ -1,15 +1,32 @@
 package com.egorlins.springboot.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
+@Table(
+        name = "products",
+        schema = "ecommerce",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                    name= "sku_unique",
+                    columnNames = "stock_keeping_unit"
+                )/*,
+                @UniqueConstraint(
+                    name= "name_unique",
+                    columnNames = "name"
+                )*/
+})
 public class Product {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "stock_keeping_unit", nullable = false)
     private String sku; //stock keeping unit
+
+    @Column(nullable = false /* , length = 200*/)
     private String name;
     private String description;
     private BigDecimal price;
