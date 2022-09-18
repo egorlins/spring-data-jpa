dafa73a496538bd971361535a2a8e30757c5df23
Jegor <egorlins@hotmail.com>
2022-09-18 13:00:47 +0300

    Added several common CRUD requests using testcase

--- src/test/java/com/egorlins/springboot/repository/ProductRepositoryTest.java
+++ src/test/java/com/egorlins/springboot/repository/ProductRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -36,5 +37,96 @@ class ProductRepositoryTest {
         //display Product info
         System.out.println(savedObject.getId());
         System.out.println(savedObject.toString());
+    }
+
+    @Test
+    void updateUsingSaveMethod(){
+
+        //find or retrieve any entity by ID
+        Long id = 1L;
+        Product product = productRepository.findById(id).get();
+
+        //update entity
+        product.setName("updated product 1");
+        product.setDescription("updated product 1 description");
+
+        //save
+        productRepository.save(product);
+    }
+
+    @Test
+    void findByIdMethod(){
+        Long id = 1L;
+        Product product = productRepository.findById(id).get();
+    }
+
+    @Test
+    void saveAllMethod(){
+        Product product = new Product();
+        product.setName("product2");
+        product.setDescription("product 3 description");
+        product.setSku("100ABCD");
+        product.setPrice(new BigDecimal(200));
+        product.setActive(true);
+        product.setImageUrl("product2.png");
+
+        Product product3 = new Product();
+        product3.setName("product3");
+        product3.setDescription("product 3 description");
+        product3.setSku("300ABCD");
+        product3.setPrice(new BigDecimal(300));
+        product3.setActive(true);
+        product3.setImageUrl("product3.png");
+
+        productRepository.saveAll(List.of(product,product3));
+    }
+
+    @Test
+    void findAllMethod(){
+        List<Product> products = productRepository.findAll();
+
+        products.forEach((product)->{
+            System.out.println(product);
+        });
+    }
+
+    @Test
+    void deleteByIdMethod(){
+        Long id = 1L;
+        productRepository.deleteById(id);
+    }
+    @Test
+    void deleteMethod(){
+
+        //find or retrieve any entity by ID
+        Long id = 2L;
+        Product product = productRepository.findById(id).get();
+
+        //delete entity
+        productRepository.delete(product);
+    }
+
+    @Test
+    void deleteAllMethod(){
+        productRepository.deleteAll();
+    }
+
+    @Test
+    void deleteAllMethod2(){
+        Product product1 = productRepository.findById(5L).get();
+        Product product2 = productRepository.findById(6L).get();
+        productRepository.deleteAll(List.of(product1, product2));
+    }
+
+    @Test
+    void countMethod(){
+        long count = productRepository.count();
+        System.out.println(count);
+    }
+
+    @Test
+    void existsByIdMethod2(){
+        System.out.println(productRepository.existsById(5L));
+        System.out.println(productRepository.existsById(9L));
     }
 }
